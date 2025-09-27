26c9266da95ef4c10ddcc6bd49a5596ce0bc269e
Jaspher Alcantara <103328596+TheUnholy1@users.noreply.github.com>
2025-09-27 19:33:17 +0800

    Add files via upload
    A basic tic tac toe game created by me

---
+++ tictactoe.py
@@ -0,0 +1,68 @@
+player = 'X'
+winner = None
+
+board = [' ' for x in range(9)]
+
+def print_board():
+    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print("---------")
+    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print("---------")
+    print(f"{board[6]} | {board[7]} | {board[8]}")
+    
+
+def check_win():
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != ' ':
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != ' ':
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != ' ':
+        winner = board[6]
+        return True
+    elif board[0] == board[3] == board[6] and board[0] != ' ':
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != ' ':
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != ' ':
+        winner = board[2]
+        return True
+    elif board[0] == board[4] == board[8] and board[0] != ' ':
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != ' ':
+        winner = board[2]
+        return True 
+    else:
+        return False
+
+
+
+while True:
+    print_board()
+    attack = int(input("Enter your move (1-9): "))
+    if attack == 0:
+        print("Game exited.")
+        break
+    if attack >=1 and attack <=9 and board[attack-1] == ' ':
+        board[attack-1] = player
+        if player == 'X':
+            player = 'O'
+        else:
+            player = 'X'
+    else:
+        print("Invalid move. Try again.")
+    if check_win():
+        print(f"Player {winner} wins!")
+        print_board()
+        break
+    if ' ' not in board:
+        print("It's a tie!")
+        print_board()
+        break
+    
+print("Game over.")
