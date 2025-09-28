f6fdee0965a92f5911a9de0b29a67fa124c9960e
Jaspher Alcantara <103328596+TheUnholy1@users.noreply.github.com>
2025-09-28 08:51:37 +0800

    Update tictactoe.py
    Better check win condition function. 
    Improved the bug where in If its a tie the computer wont need to wait for his turn

--- tictactoe.py
+++ tictactoe.py
@@ -27,32 +27,22 @@ def print_board():
 def check_win():
     """This is a function to check if there is a winner."""
     global GAME_WINNER
-    if board[0] == board[1] == board[2] and board[0] != ' ':
-        GAME_WINNER = board[0]
-        return True
-    elif board[3] == board[4] == board[5] and board[3] != ' ':
-        GAME_WINNER = board[3]
-        return True
-    elif board[6] == board[7] == board[8] and board[6] != ' ':
-        GAME_WINNER = board[6]
-        return True
-    elif board[0] == board[3] == board[6] and board[0] != ' ':
-        GAME_WINNER = board[0]
-        return True
-    elif board[1] == board[4] == board[7] and board[1] != ' ':
-        GAME_WINNER = board[1]
-        return True
-    elif board[2] == board[5] == board[8] and board[2] != ' ':
-        GAME_WINNER = board[2]
-        return True
-    elif board[0] == board[4] == board[8] and board[0] != ' ':
-        GAME_WINNER = board[0]
-        return True
-    elif board[2] == board[4] == board[6] and board[2] != ' ':
-        GAME_WINNER = board[2]
-        return True 
-    else:
-        return False
+    win_combinations = [
+        (0, 1, 2),  # rows
+        (3, 4, 5),
+        (6, 7, 8),
+        (0, 3, 6),  # columns
+        (1, 4, 7),
+        (2, 5, 8),
+        (0, 4, 8),  # diagonals
+        (2, 4, 6)
+    ]
+
+    for a, b, c in win_combinations:
+        if board[a] == board[b] == board[c] and board[a] != ' ':
+            GAME_WINNER = board[a]
+            return True
+    return False
 
 def computer_move():
     """Function for the computer to make a move."""
@@ -60,8 +50,8 @@ def computer_move():
         position = random.randint(0, 8)
         if board[position] == ' ':
             board[position] = 'O'
-        switch_player()
-        break
+            switch_player()
+            break
 
 def switch_player():
     """Function to switch the current player."""
@@ -72,16 +62,28 @@ def switch_player():
         CURRENT_PLAYER = 'X'   
 def player_input():
     """Function to take player input"""
-    global CURRENT_PLAYER
+    global CURRENT_PLAYER, GAME_RUNNING
     try:
         attack = int(input("Enter your move (1-9, 0 to quit): "))
     except ValueError:
         print("Invalid input. Please enter a number.")
         return False
     if attack == 0:
-        return True # Exit condition
-    if 1 <= attack <= 9 and board[attack-1] == ' ':
-        board[attack-1] = CURRENT_PLAYER
+        return True  # Exit condition
+    if 1 <= attack <= 9 and board[attack - 1] == ' ':
+        board[attack - 1] = CURRENT_PLAYER
+        if check_win():
+            clear_console()
+            print(f"Player {CURRENT_PLAYER} wins!")
+            print_board()
+            GAME_RUNNING = False
+            return True
+        if ' ' not in board:
+            clear_console()
+            print("It's a tie!")
+            print_board()
+            GAME_RUNNING = False
+            return True
         if CURRENT_PLAYER == 'X':
             CURRENT_PLAYER = 'O'
             computer_move()
@@ -89,19 +91,10 @@ def player_input():
     else:
         print("Invalid move. Try again.")
     return False
+
 while GAME_RUNNING:
     print_board()
     EXIT_CONDITION = player_input()
     if EXIT_CONDITION:
         print('Game Over. Exiting...')
-        GAME_RUNNING = False
-    if check_win():
-        clear_console()
-        print(f"Player {GAME_WINNER} wins!")
-        print_board()
-        GAME_RUNNING = False
-    if ' ' not in board:
-        clear_console()
-        print("It's a tie!")
-        print_board()
         GAME_RUNNING = False
